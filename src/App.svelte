aa01f30867508b1a9f0de768142266616e1fd114
Avni Onur Pehlivan <avnionur@gmail.com>
2019-06-07 02:01:01 +0300

    0.1.0

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	let name = "Onur Pehlivan";
+	let points = 100;
+	let showControls = false;
+
+	const addPoint = () => points += 1;
+	const removePoint = () => points -= 1;
+	const toggleControls = () => (showControls = !showControls);
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+
+	h3 {
+		margin-bottom: 12px;
+	}
+</style>
+
+<div class="container">
+	<div class="card">
+		<h1>
+			{name}
+			<button class="btn btn-sm" on:click={toggleControls}>
+			{#if showControls}-{:else}+{/if}
+			</button>
+		</h1>
+		<h3>Points: {points}</h3>
+		{#if showControls}
+		<button class="btn" on:click={addPoint}>+1</button>
+		<button class="btn btn-dark" on:click={removePoint}>-1</button>
+		<input type="number" bind:value={points}>
+		{/if}
+	</div>
+</div>
